d40e5c7b459d3771ce54ecd763b62cfed251a6a2
Bansi-Bhimani <90316950+Bansi-Bhimani@users.noreply.github.com>
2021-10-13 18:29:29 +0530

    Add files via upload

---
+++ Module 2/Code/Looping/ForeachLoop/ForeachLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] IntArray = { 1,9,8,6,45,77};
+            foreach (int i in IntArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
